01456e7d118842a46dbdffc3207201cccb23418b
Slava Borysyuk <yaroslavborysyuk1@gmail.com>
2025-03-18 16:15:41 -0400

    Fixed privacy editing in the backend

--- django_project/core/serializers.py
+++ django_project/core/serializers.py
@@ -7,14 +7,18 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ['username', 'first_name', 'private', 'profile_picture', 'profile_hue']
         
     def update(self, instance, validated_data):
-       
+        
+        # If no new profile picture, keep the old one
         if validated_data["profile_picture"] is None:
             validated_data["profile_picture"] = instance.profile_picture
         
-        new_username = validated_data.get("username", instance.username)
-
         # Check if the username is changing and if it's already taken
+        new_username = validated_data.get("username", instance.username)
         if new_username != instance.username and User.objects.filter(username=new_username).exists():
             raise serializers.ValidationError({"username": "A user with that username already exists."})
+        
+        # Revert the privacy if user checked the box in frontend
+        if validated_data["private"] == True:
+           validated_data["private"] = not instance.private
 
         return super().update(instance, validated_data)
