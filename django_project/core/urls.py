31f39738b14c2a148b353bfd41ce4432166ccf25
Slava Borysyuk <yaroslavborysyuk1@gmail.com>
2025-04-16 13:37:44 -0400

    Added the new add-friends url into urls.py

--- django_project/core/urls.py
+++ django_project/core/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import (
     home_view, register_view, login_view, logout_view, profile_view, 
-    collection_view, add_collection_view, search_view , UserUpdate, CollectionUpdate, SaveToCollection, DeleteFromCollection
+    collection_view, add_collection_view, search_view, add_friends_view ,UserUpdate, CollectionUpdate, SaveToCollection, DeleteFromCollection
 )
 
 app_name = "core"
@@ -18,5 +18,6 @@ urlpatterns = [
     path("profile/<str:username>/collection/<str:code>/update", CollectionUpdate.as_view(), name="update_collection"),
     path("profile/<str:username>/collection/<str:code>/delete/<str:type>/<str:music>", DeleteFromCollection.as_view(), name="delete_music"),
     path("save", SaveToCollection.as_view(), name="save"),
-    path("search", search_view, name="search")
+    path("search", search_view, name="search"),
+    path("add-friends", add_friends_view, name="add_friends")
 ]
