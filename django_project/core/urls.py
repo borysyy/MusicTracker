bb78df7556f68ba74a3c523a0f02f5baecb80efb
Slava Borysyuk <yaroslavborysyuk1@gmail.com>
2025-04-22 15:10:58 -0400

    New url for all of your current requests

--- django_project/core/urls.py
+++ django_project/core/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import (
     home_view, register_view, login_view, logout_view, profile_view, 
     collection_view, add_collection_view, search_view, search_friends_view, 
+    get_current_requests_view, 
     UserUpdate, CollectionUpdate, SaveToCollection, DeleteFromCollection,
     RequestFriendView, CancelFriendRequestView, AcceptFriendRequestView, 
     RejectFriendRequestView, RemoveFriendView,
@@ -19,8 +20,8 @@ urlpatterns = [
     # Urls for user profile pages
     path("profile/<str:username>", profile_view, name="profile"),
     path("profile/<str:username>/update", UserUpdate.as_view(), name="update_profile"),
-    path("profile/<str:username>/collection/<str:code>", collection_view, name="collection"),
     path("profile/<str:username>/collection/add", add_collection_view, name="add_collection"),
+    path("profile/<str:username>/collection/<str:code>", collection_view, name="collection"),
     path("profile/<str:username>/collection/<str:code>/update", CollectionUpdate.as_view(), name="update_collection"),
     path("profile/<str:username>/collection/<str:code>/delete/<str:type>/<str:music>", DeleteFromCollection.as_view(), name="delete_music"),
     
@@ -30,6 +31,7 @@ urlpatterns = [
     
     # Urls for friend feature
     path("add-friends", search_friends_view, name="add_friends"),
+    path("current-requests", get_current_requests_view, name="current_requests"),
     path("request-friend/<str:username>", RequestFriendView.as_view(), name="requesting_friend"),
     path("cancel-request/<str:username>", CancelFriendRequestView.as_view(), name="cancel_friend_request"),
     path("accept-request/<str:username>", AcceptFriendRequestView.as_view(), name="accept_friend_request"),
