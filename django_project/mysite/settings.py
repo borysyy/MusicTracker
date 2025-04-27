2126398e187a6d31690bd49013073cd0b6990166
Slava Borysyuk <yaroslavborysyuk1@gmail.com>
2025-04-27 18:20:35 -0400

    Changed key to django secret key

--- django_project/mysite/settings.py
+++ django_project/mysite/settings.py
@@ -34,7 +34,7 @@ STATICFILES_DIRS = [
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("KEY")
+SECRET_KEY = config("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=False)
