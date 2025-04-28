4e7b6ae9008b9fc4f98eb49793ecbd0f8f9061a0
Slava Borysyuk <yaroslavborysyuk1@gmail.com>
2025-04-28 00:07:19 -0400

    Changes from the prod server

--- django_project/mysite/settings.py
+++ django_project/mysite/settings.py
@@ -37,7 +37,7 @@ STATICFILES_DIRS = [
 SECRET_KEY = config("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config("DEBUG", default=False)
+DEBUG = config("DEBUG", default=False, cast=bool)
 
 
 ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="localhost").split(",")
